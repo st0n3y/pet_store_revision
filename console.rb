3f2848cf26a122ff6d3163518894613d768412fc
David MacKintosh <dgawmackintosh@gmail.com>
2016-05-11 22:49:35 +0100

    Modifies runner

--- console.rb
+++ console.rb
@@ -3,7 +3,7 @@ require_relative( './models/pet_store.rb' )
 
 require( 'pry-byebug' )
 
-pet_store1 = PetStore.new( { 'name' => 'Shit Ass Pet Fuckers', 'address' => '123 Fake Street', 'stock_type' => 'exotic' } ).save()
+pet_store1 = PetStore.new( { 'name' => 'Bark Side of the Moon', 'address' => '123 Fake Street', 'stock_type' => 'exotic' } ).save()
 
 pet1 = Pet.new( { 'name' => 'Darth', 'type' => 'pygmy marmoset', 'image' => 'http://images.wisegeek.com/small-marmoset.jpg', 'pet_store_id' => pet_store1.id } ).save()
 
